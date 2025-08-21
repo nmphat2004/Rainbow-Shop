e6963937e56ab8ae04553556c6f8cffe73a38fa3
Phat <2251120435@ut.edu.vn>
2025-08-21 22:42:20 +0700

    add navbar with searchbar

--- frontend/src/components/Common/Header.jsx
+++ frontend/src/components/Common/Header.jsx
@@ -1,9 +1,11 @@
 import Topbar from '../Layout/Topbar';
+import Navbar from './Navbar';
 
 const Header = () => {
 	return (
-		<header>
+		<header className='border-b border-gray-200'>
 			<Topbar />
+			<Navbar />
 		</header>
 	);
 };
