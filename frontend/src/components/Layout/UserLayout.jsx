4f27d5f15bac8bb6965030408b1d72c291f820ec
Phat <2251120435@ut.edu.vn>
2025-08-25 23:32:55 +0700

    add footer component

--- frontend/src/components/Layout/UserLayout.jsx
+++ frontend/src/components/Layout/UserLayout.jsx
@@ -1,10 +1,11 @@
-import React from 'react';
+import Footer from '../Common/Footer';
 import Header from '../Common/Header';
 
 const UserLayout = () => {
 	return (
 		<>
 			<Header />
+			<Footer />
 		</>
 	);
 };
