2c6d860931299e07460bdd1982e28eaba85bffba
Phat <2251120435@ut.edu.vn>
2025-08-29 23:27:27 +0700

    add new arrivals components with carousel'

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,11 +1,13 @@
 import Hero from '../components/Layout/Hero';
 import GenderCollectionSection from '../components/Products/GenderCollectionSection';
+import NewArrivals from '../components/Products/NewArrivals';
 
 const Home = () => {
 	return (
 		<div>
 			<Hero />
 			<GenderCollectionSection />
+			<NewArrivals />
 		</div>
 	);
 };
