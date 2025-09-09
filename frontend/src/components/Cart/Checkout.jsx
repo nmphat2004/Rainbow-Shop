a2251bda9ed89394d35632186fa734247612f592
Phat <2251120435@ut.edu.vn>
2025-09-09 21:27:44 +0700

    complete checkout page

--- frontend/src/components/Cart/Checkout.jsx
+++ frontend/src/components/Cart/Checkout.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useRef, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import PayPalButton from './PayPalButton';
 
@@ -191,6 +191,41 @@ const Checkout = () => {
 						)}
 					</div>
 				</form>
+			</div>
+			{/* Right Section */}
+			<div className='bg-gray-50 p-6 rounded-lg'>
+				<h3 className='text-lg mb-4'> Order Summary</h3>
+				<div className='border-t py-4 mb-4'>
+					{cart.products.map((product, index) => (
+						<div key={index} className='flex items-start justify-between'>
+							<div className='flex items-start'>
+								<img
+									src={product.image}
+									alt={product.name}
+									className='w-20 h-24 object-cover mr-4'
+								/>
+								<div>
+									<h3 className='text-md'>{product.name}</h3>
+									<p className='text-gray-500'>Size: {product.size}</p>
+									<p className='text-gray-500'>Color: {product.color}</p>
+								</div>
+							</div>
+							<p className='text-xl'>${product.price?.toLocaleString()}</p>
+						</div>
+					))}
+				</div>
+				<div className='flex justify-between items-center text-lg mb-4'>
+					<p>Subtotal</p>
+					<p>${cart.totalPrice?.toLocaleString()}</p>
+				</div>
+				<div className='flex justify-between items-center text-lg'>
+					<p>Shipping</p>
+					<p>Free</p>
+				</div>
+				<div className='flex items-center justify-between text-lg mt-4 border-t pt-4'>
+					<p>Total</p>
+					<p>${cart.totalPrice?.toLocaleString()}</p>
+				</div>
 			</div>
 		</div>
 	);
