070f382548a69ff136c6cb15d4f35df425c06d3c
Phat <2251120435@ut.edu.vn>
2025-09-10 23:27:02 +0700

    add admin layout and sidebar components with navigation link

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -11,6 +11,7 @@ import Checkout from './components/Cart/Checkout';
 import OrderConfirmationPage from './pages/OrderConfirmationPage';
 import OrderDetailsPage from './pages/OrderDetailsPage';
 import MyOrdersPage from './pages/MyOrdersPage';
+import AdminLayout from './components/Admin/AdminLayout';
 
 const App = () => {
 	return (
@@ -32,6 +33,7 @@ const App = () => {
 					<Route path='order/:id' element={<OrderDetailsPage />} />
 					<Route path='my-orders' element={<MyOrdersPage />} />
 				</Route>
+				<Route path='/admin' element={<AdminLayout />} />
 			</Routes>
 		</BrowserRouter>
 	);
---
+++ frontend/src/components/Admin/AdminSidebar.jsx
@@ -0,0 +1,4 @@
+const AdminSidebar = () => {
+	return <div>AdminSidebar</div>;
+};
+export default AdminSidebar;
