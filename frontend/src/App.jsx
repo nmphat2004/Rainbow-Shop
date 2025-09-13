ee9a4b37ab75251e8f3cb9c57b3f979b092c40ee
Phat <2251120435@ut.edu.vn>
2025-09-13 09:32:17 +0700

    add order management component and complete admin page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -16,6 +16,7 @@ import AdminHomePage from './pages/AdminHomePage';
 import UserManagement from './components/Admin/UserManagement';
 import ProductManagement from './components/Admin/ProductManagement';
 import EditProductPage from './components/Admin/EditProductPage';
+import OrderManagement from './components/Admin/OrderManagement';
 
 const App = () => {
 	return (
@@ -42,6 +43,7 @@ const App = () => {
 					<Route path='users' element={<UserManagement />} />
 					<Route path='products' element={<ProductManagement />} />
 					<Route path='products/:id/edit' element={<EditProductPage />} />
+					<Route path='orders' element={<OrderManagement />} />
 				</Route>
 			</Routes>
 		</BrowserRouter>
