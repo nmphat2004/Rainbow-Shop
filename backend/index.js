7acb11544ac6ac715fd662dbab8d7a4c1696deb7
Phat <2251120435@ut.edu.vn>
2025-09-13 23:35:10 +0700

    connect mongodb

--- backend/index.js
+++ backend/index.js
@@ -1,11 +1,17 @@
 const express = require('express')
 const cors = require('cors')
+const dotenv = require('dotenv')
+const connectDB = require('./config/db')
 
 const app = express()
 app.use(express.json())
 app.use(cors())
 
-const PORT = 3000
+dotenv.config()
+const PORT = process.env.PORT || 3000
+
+// Connect to MongoDB
+connectDB()
 
 app.get('/', (req, res) => {
   res.send('Hello world')
