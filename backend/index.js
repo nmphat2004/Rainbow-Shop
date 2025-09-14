236947992af86ef65e7233aded82852dceaa4af4
Phat <2251120435@ut.edu.vn>
2025-09-14 20:37:42 +0700

    api user register

--- backend/index.js
+++ backend/index.js
@@ -2,6 +2,7 @@ const express = require('express')
 const cors = require('cors')
 const dotenv = require('dotenv')
 const connectDB = require('./config/db')
+const userRoutes = require('./routes/user.routes')
 
 const app = express()
 app.use(express.json())
@@ -16,6 +17,9 @@ connectDB()
 app.get('/', (req, res) => {
   res.send('Hello world')
 })
+
+// API Routes
+app.use('/api/users', userRoutes)
 
 app.listen(PORT, () => {
   console.log(`App is running on port: ${PORT}`);
--- backend/models/User.js
+++ backend/models/User.js
@@ -31,7 +31,7 @@ const userSchema = new mongoose.Schema({
 // Password Hash middleware
 userSchema.pre('save', async function (next) {
   if (!this.isModified('password')) return next()
-  const salt = bcrypt.genSalt(10)
+  const salt = await bcrypt.genSalt(10)
   this.password = await bcrypt.hash(this.password, salt)
   next()
 })
