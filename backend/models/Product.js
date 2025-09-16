752eba56608b26bdb8436264b3a784635a948d82
Phat <2251120435@ut.edu.vn>
2025-09-16 23:35:51 +0700

    create product model

---
+++ backend/models/Product.js
@@ -0,0 +1,104 @@
+const mongoose = require('mongoose')
+
+const productSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+    trim: true
+  },
+  price: {
+    type: Number,
+    required: true
+  },
+  discountPrice: {
+    type: Number,
+  },
+  countInStock: {
+    type: Number,
+    required: true,
+    default: 0
+  },
+  sku: {
+    type: String,
+    unique: true,
+    required: true
+  },
+  category: {
+    type: String,
+    required: true
+  },
+  brand: {
+    type: String
+  },
+  sizes: {
+    type: [String],
+    required: true
+  },
+  colors: {
+    type: [String],
+    required: true
+  },
+  collections: {
+    type: String,
+    required: true
+  },
+  material: {
+    type: String,
+  },
+  gender: {
+    type: String,
+    enum: ['Men', 'Women', 'Unisex']
+  },
+  images: [{
+    url: {
+      type: String,
+      required: true
+    },
+    altText: {
+      type: String
+    }
+  }],
+  isFeatured: {
+    type: Boolean,
+    default: false,
+  },
+  isPublish: {
+    type: Boolean,
+    default: false
+  },
+  rating: {
+    type: Number,
+    default: 0
+  },
+  numReviews: {
+    type: String,
+    default: 0
+  },
+  tags: [String],
+  user: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User',
+    required: true
+  },
+  metaTitle: {
+    type: String
+  },
+  metaDescription: {
+    type: String
+  },
+  metaKeywords: {
+    type: String
+  },
+  dimensions: {
+    length: Number,
+    width: Number,
+    height: Number
+  },
+  weight: Number
+}, {
+  timestamps: true
+})
+
+const Product = mongoose.model('Product', productSchema)
+
+module.exports = Product
