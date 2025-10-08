6975de48a029a6e5aeb1c7542895c62f1f438943
Phat <2251120435@ut.edu.vn>
2025-10-08 22:42:49 +0700

    create checkoutSlice to handle checkout session

--- frontend/src/redux/store.js
+++ frontend/src/redux/store.js
@@ -1,17 +1,17 @@
-import {
-  configureStore
-} from '@reduxjs/toolkit'
+import { configureStore } from '@reduxjs/toolkit';
 
-import authReducer from './slices/authSlice'
-import productsReducer from './slices/productsSlice'
-import cartReducer from './slices/cartSlice'
+import authReducer from './slices/authSlice';
+import productsReducer from './slices/productsSlice';
+import cartReducer from './slices/cartSlice';
+import checkoutReducer from './slices/checkoutSlice';
 
 const store = configureStore({
-  reducer: {
-    auth: authReducer,
-    products: productsReducer,
-    cart: cartReducer
-  }
-})
+	reducer: {
+		auth: authReducer,
+		products: productsReducer,
+		cart: cartReducer,
+		checkout: checkoutReducer,
+	},
+});
 
-export default store
+export default store;
