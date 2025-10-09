57bebcb8ae26597493c4db438ba733ab6c292928
Phat <2251120435@ut.edu.vn>
2025-10-09 23:26:04 +0700

    Add adminOrderSlice to manage admin order operations

---
+++ frontend/src/redux/slices/adminOrderSlice.js
@@ -0,0 +1,111 @@
+import { createAsyncThunk, createSlice } from '@reduxjs/toolkit';
+import axios from 'axios';
+
+// Async thunk to get all orders
+export const fetchAdminOrders = createAsyncThunk(
+	'adminOrders/fetchAdminOrders',
+	async (_, { rejectWithValue }) => {
+		try {
+			const response = await axios.get(
+				`${import.meta.env.VITE_BACKEND_URL}/api/admin/orders`,
+				{
+					headers: {
+						Authorization: `Bearer ${localStorage.getItem('userToken')}`,
+					},
+				}
+			);
+			return response.data;
+		} catch (error) {
+			return rejectWithValue(error.response.data);
+		}
+	}
+);
+
+// Async thunk to update order status
+export const updateOrderStatus = createAsyncThunk(
+	'adminOrders/fetchAdminOrders',
+	async ({ id, status }, { rejectWithValue }) => {
+		try {
+			const response = await axios.put(
+				`${import.meta.env.VITE_BACKEND_URL}/api/admin/orders/${id}`,
+				{ status },
+				{
+					headers: {
+						Authorization: `Bearer ${localStorage.getItem('userToken')}`,
+					},
+				}
+			);
+			return response.data;
+		} catch (error) {
+			return rejectWithValue(error.response.data);
+		}
+	}
+);
+
+// Async thunk to delete an order
+export const deleteOrder = createAsyncThunk(
+	'adminOrders/fetchAdminOrders',
+	async (id, { rejectWithValue }) => {
+		try {
+			await axios.delete(
+				`${import.meta.env.VITE_BACKEND_URL}/api/admin/orders/${id}`,
+
+				{
+					headers: {
+						Authorization: `Bearer ${localStorage.getItem('userToken')}`,
+					},
+				}
+			);
+			return id;
+		} catch (error) {
+			return rejectWithValue(error.response.data);
+		}
+	}
+);
+
+const adminOrderSlice = createSlice({
+	name: 'adminOrders',
+	initialState: {
+		orders: [],
+		totalOrder: 0,
+		totalSales: 0,
+		loading: false,
+		error: null,
+	},
+	reducers: {},
+	extraReducers: (builder) => {
+		builder
+			.addCase(fetchAdminOrders.pending, (state) => {
+				state.loading = true;
+				state.error = null;
+			})
+			.addCase(fetchAdminOrders.fulfilled, (state, action) => {
+				state.loading = false;
+				state.orders = Array.isArray(action.payload) ? action.payload : [];
+				state.totalOrder = state.orders.length;
+
+				const totalSales = action.payload.reduce((acc, order) => {
+					return acc + order.totalPrice;
+				}, 0);
+				state.totalSales = totalSales;
+			})
+			.addCase(fetchAdminOrders.pending, (state, action) => {
+				state.loading = false;
+				state.error = action.payload.message;
+			})
+			.addCase(updateOrderStatus.fulfilled, (state, action) => {
+				const updatedOrder = action.payload;
+				const orderIndex = state.orders.findIndex(
+					(order) => order._id === updatedOrder._id
+				);
+				if (orderIndex !== -1) state.orders[orderIndex] = updatedOrder;
+			})
+			.addCase(deleteOrder.fulfilled, (state, action) => {
+				state.orders = state.orders.filter(
+					(order) => order._id !== action.payload
+				);
+			});
+	},
+});
+
+export default adminOrderSlice.reducer;
